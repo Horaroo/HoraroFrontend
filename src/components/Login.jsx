35074a68ba1e26f050b89ae06c905b47621e25ad
Arsen050 <64630664+Arsen050@users.noreply.github.com>
2022-08-13 19:52:50 +0300

    feat: completed the layout for Login and Signup

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,22 +1,62 @@
-import { Box, TextField, Button } from '@material-ui/core'
+import { Box, TextField, Button, InputAdornment } from '@material-ui/core'
+import LockIcon from '@material-ui/icons/Lock'
+import PersonIcon from '@material-ui/icons/Person'
+import { NavLink } from 'react-router-dom'
 const Login = () => {
     return (
         <div className="auth__form-layout">
             <Box component="form" className="auth__form" spacing={2}>
-                <h2 className="auth__form-title">Welcome</h2>
+                <h2 className="auth__form-title">Log in</h2>
                 <TextField
-                    type="email"
-                    placeholder="email"
+                    variant="outlined"
+                    type="text"
+                    placeholder="Username"
                     className="auth__form-input"
+                    size="small"
+                    InputProps={{
+                        endAdornment: (
+                            <InputAdornment
+                                position="start"
+                                className="input-icon"
+                            >
+                                <LockIcon />
+                            </InputAdornment>
+                        ),
+                    }}
                 />
+
                 <TextField
+                    variant="outlined"
                     type="password"
-                    placeholder="password"
+                    placeholder="Password"
                     className="auth__form-input"
+                    size="small"
+                    InputProps={{
+                        endAdornment: (
+                            <InputAdornment
+                                position="start"
+                                className="input-icon"
+                            >
+                                <PersonIcon />
+                            </InputAdornment>
+                        ),
+                    }}
                 />
-                <Button color="primary" variant="contained" size="small">
-                    Login
+                <Button
+                    color="primary"
+                    variant="contained"
+                    size="large"
+                    style={{ marginBottom: 10 }}
+                >
+                    Log in
                 </Button>
+                <NavLink className="link link--light mb-10" to="/login">
+                    Forgot Password?
+                </NavLink>
+                <NavLink className="link link--not-hover" to="/signup">
+                    Not registered yet?
+                    <span className="link--light"> Sign Up here!</span>
+                </NavLink>
             </Box>
         </div>
     )
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,5 +1,110 @@
+import { Box, TextField, Button, InputAdornment } from '@material-ui/core'
+import LockIcon from '@material-ui/icons/Lock'
+import PersonIcon from '@material-ui/icons/Person'
+import EmailIcon from '@material-ui/icons/Email'
+import { NavLink } from 'react-router-dom'
+import Autocomplete from '@material-ui/lab/Autocomplete'
+import { useState } from 'react'
+
+const GROUP_TEST = [
+    { id: 1, name: 'У851' },
+    { id: 2, name: 'У836' },
+    { id: 3, name: 'У736' },
+]
 const SignUp = () => {
-    return <div>SignUp</div>
+    const [selectedGroup, setSelectedGroup] = useState(null)
+    return (
+        <div className="auth__form-layout">
+            <Box component="form" className="auth__form" spacing={2}>
+                <h2 className="auth__form-title">Sign up</h2>
+                <TextField
+                    variant="outlined"
+                    type="text"
+                    placeholder="Username"
+                    className="auth__form-input"
+                    size="small"
+                    InputProps={{
+                        endAdornment: (
+                            <InputAdornment
+                                position="start"
+                                className="input-icon"
+                            >
+                                <LockIcon />
+                            </InputAdornment>
+                        ),
+                    }}
+                />
+
+                <TextField
+                    variant="outlined"
+                    type="email"
+                    placeholder="Email"
+                    className="auth__form-input"
+                    size="small"
+                    InputProps={{
+                        endAdornment: (
+                            <InputAdornment
+                                position="start"
+                                className="input-icon"
+                            >
+                                <EmailIcon />
+                            </InputAdornment>
+                        ),
+                    }}
+                />
+
+                <TextField
+                    variant="outlined"
+                    type="password"
+                    placeholder="Password"
+                    className="auth__form-input"
+                    size="small"
+                    InputProps={{
+                        endAdornment: (
+                            <InputAdornment
+                                position="start"
+                                className="input-icon"
+                            >
+                                <PersonIcon />
+                            </InputAdornment>
+                        ),
+                    }}
+                />
+                <Autocomplete
+                    className="mb-35"
+                    id="nba teams"
+                    options={GROUP_TEST}
+                    renderInput={(params) => (
+                        <TextField
+                            {...params}
+                            label="Group"
+                            variant="outlined"
+                            size="small"
+                        />
+                    )}
+                    getOptionLabel={(option) => option.name}
+                    style={{ width: '100%' }}
+                    value={selectedGroup}
+                    onChange={(_event, group) => {
+                        setSelectedGroup(group)
+                    }}
+                />
+
+                <Button
+                    color="primary"
+                    variant="contained"
+                    size="large"
+                    style={{ marginBottom: 10 }}
+                >
+                    Sign up
+                </Button>
+                <NavLink className="link  mb-10" to="/login">
+                    Have an account?
+                    <span className="link--light"> Sign in.</span>
+                </NavLink>
+            </Box>
+        </div>
+    )
 }
 
 export default SignUp

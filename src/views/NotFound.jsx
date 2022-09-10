5d1629b48bff5a35e75c2ecc6e47f14c804c7d1d
eskenderov <eskenderovar@yandex.ru>
2022-09-10 16:44:44 +0300

    feat: add notfound image

--- src/views/NotFound.jsx
+++ src/views/NotFound.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
-
+import NotFoundImage from 'assets/images/404.jpg'
+import { useEffect } from 'react'
 const NotFound = () => {
-    return <div>NotFound</div>
+    useEffect(() => {
+        const body = document.body
+        body.style.background = '#ffff'
+    }, [])
+    return (
+        <div className="page notfound">
+            <div className="notfound__content">
+                <img src={NotFoundImage} alt="notfound-image" />
+            </div>
+        </div>
+    )
 }
 
 export default NotFound
